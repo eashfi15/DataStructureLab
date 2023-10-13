44320ae230dae92f767b9c1aed329d9be8327413
eashfi15 <147539009+eashfi15@users.noreply.github.com>
2023-10-13 22:19:12 +0600

    BankAccount.cpp

--- Labtask3(2).cpp
+++ Labtask3(2).cpp
@@ -17,6 +17,26 @@ cin>>Account.AccHolderName;
 getline(cin,Account.AccHolderName);
 cout<<"Enter account balance: "<<endl;
 cin>>Account.AccBalance;
-return 0;
+    
+returndouble depositAmount;
+cout<<"Enter the ammount to deposit: "<<endl;
+cin>>depositAmount;
+Account.AccBalance+depositAmount =depositeAmmount;
+
+double withdrawAmount;
+cout<<"Enter the amount to withdraw: "<<endl;
+cin>>withdrawAmount;
+if(withdrawAmount<=Account.AccBalance){
+
+Account.AccBalance-withdrawAmount=withdrawAmount;
+}
+else{
+    cout<<"Insufficient Funds."<<endl;
+}
+
+cout<<"Account Number:"<<Account.AccNum<<endl;
+cout<<"Account Holder's Name: "<<Account.AccHolderName<<endl;
+cout<<"Account Balance: "<<Account.AccBalance<<endl;
+ 0;
 }
 
